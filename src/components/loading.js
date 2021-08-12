676c64440b16b5dc5bf5ddfa4e475923372c224d
Daniel Jungstedt <daniel.jungs@ibm.com>
2021-08-11 22:13:51 -0300

    Add first style for loading component

--- src/components/loading.js
+++ src/components/loading.js
@@ -4,9 +4,11 @@ function WithListLoading(Component) {
   return function WihLoadingComponent({ isLoading, ...props }) {
     if (!isLoading) return <Component {...props} />;
     return (
-      <p style={{ textAlign: 'center', fontSize: '30px' }}>
-        Hold on, fetching data may take some time :)
-      </p>
+      <div className='loading-container'>
+        <p style={{ textAlign: 'center', fontSize: '30px' }}>
+            Carregando Ofertas...
+        </p>
+      </div>
     );
   };
 }
